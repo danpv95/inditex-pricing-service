02308762c028754183e8738b6e4b37856edd3ae8
danpv95 <danpv95@gmail.com>
2025-05-27 09:50:34 -0500

    feat(application): implement use case logic in PriceService

--- src/main/java/com/bcnc/inditex_pricing_service/application/PriceService.java
+++ src/main/java/com/bcnc/inditex_pricing_service/application/PriceService.java
@@ -1,4 +1,29 @@
 package com.bcnc.inditex_pricing_service.application;
 
-public class PriceService {
+import com.bcnc.inditex_pricing_service.domain.exception.NoApplicablePriceException;
+import com.bcnc.inditex_pricing_service.domain.model.Price;
+import com.bcnc.inditex_pricing_service.domain.port.input.PriceUseCase;
+import com.bcnc.inditex_pricing_service.domain.port.output.PriceRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Comparator;
+
+@Service
+public class PriceService implements PriceUseCase {
+
+    private final PriceRepository priceRepository;
+
+    public PriceService(PriceRepository priceRepository) {
+        this.priceRepository = priceRepository;
+    }
+
+    @Override
+    public Price getApplicablePrice(Long productId, Long brandId, LocalDateTime applicationDate) {
+        return priceRepository.findByProductIdAndBrandId(productId, brandId).stream()
+                .filter(p -> !applicationDate.isBefore(p.getStartDate()) && !applicationDate.isAfter(p.getEndDate()))
+                .max(Comparator.comparingInt(Price::getPriority))
+                .orElseThrow(() -> new NoApplicablePriceException(productId, brandId));
+    }
+
 }
